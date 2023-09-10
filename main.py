63b500404355214ff427cd87cd6846d05646d5cf
aleksey <Kaloha215@gmail.com>
2023-09-10 16:14:54 +1000

    add model for trades

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI  # Основной класс для создания приложения
+from typing import List  # Доболнительный тип данных (список)
+from pydantic import BaseModel, Field  # Классы для работы с данными
 
+# Создание приложения
 app = FastAPI(
     title="Trading App"
 )
 
+# тестовая бд с пользователями
 fake_users = [
     {"id": 1, "role": "admin", "name": "Bob"},
     {"id": 2, "role": "investor", "name": "John"},
@@ -11,6 +15,33 @@ fake_users = [
 ]
 
 
+# получение пользователя по id
 @app.get("/users/{user_id}")
 def get_user(user_id):
     return [user for user in fake_users if user.get("id") == user_id]
+
+
+# тестовая бд с трейдерами
+fake_trades = [
+    {"id": 1, "user_id": 1, "currency": "BTC",
+     "side": "buy", "price": 123, "amount": 2.12},
+    {"id": 2, "user_id": 1, "currency": "BTC",
+     "side": "sell", "price": 125, "amount": 2.12},
+]
+
+
+# модель данных "Сделка"
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)  # огрничение длины строки
+    side: str
+    price: float = Field(ge=0)  # задется ограничение для ввода (>=0)
+    amount: float
+
+
+# добавление трейдера в бд
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
