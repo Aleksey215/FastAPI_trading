4d7a91a93673d6d25fc7fe9605636c84824bf810
Aleksey <kaloha215@gmail.com>
2024-06-12 12:44:15 +1000

    create module - authentification

---
+++ auth/auth.py
@@ -0,0 +1,25 @@
+"""
+Main file in autentification module
+"""
+from fastapi_users.authentication import (
+    AuthenticationBackend,
+    CookieTransport,
+    JWTStrategy
+)
+
+# choose Transport (example: cooky)
+cookie_transport = CookieTransport(cookie_max_age=3600)
+
+SECRET = "SECRET"
+
+
+# choose Strategies (example: jwt)
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
--- models/models.py
+++ models/models.py
@@ -3,9 +3,17 @@ Models for creating tables in database
 """
 from datetime import datetime, timezone
 
-from sqlalchemy import MetaData, Table, Column, \
-    Integer, String, TIMESTAMP, ForeignKey, JSON
-
+from sqlalchemy import (
+    MetaData,
+    Table,
+    Column,
+    Integer,
+    String,
+    TIMESTAMP,
+    ForeignKey,
+    JSON,
+    Boolean
+)
 
 metadata = MetaData()
 
@@ -24,8 +32,11 @@ user = Table(
     Column("id", Integer, primary_key=True),
     Column("email", String, nullable=False),
     Column("username", String, nullable=False),
-    Column("password", String, nullable=False),
+    Column("hashed_password", String, nullable=False),
     Column("registred_at", TIMESTAMP, default=datetime.now(tz=timezone.utc)),
     Column("role_id", Integer, ForeignKey("trading.role.id")),
+    Column("is_active", Boolean, default=True, nullable=False),
+    Column("is_superuser", Boolean, default=False, nullable=False),
+    Column("is_verified", Boolean, default=False, nullable=False),
     schema='trading'
 )
