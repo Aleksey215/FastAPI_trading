d4c7e9ba9f88fc9824fb22902c602bd6d1f73e0e
Aleksey <kaloha215@gmail.com>
2024-06-12 12:47:07 +1000

    add user manager

--- auth/auth.py
+++ auth/auth.py
@@ -18,6 +18,7 @@ def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
 
 
+# Creation backend
 auth_backend = AuthenticationBackend(
     name="jwt",
     transport=cookie_transport,
